ac65e2d29665a7363d8d9b8c81070d5949158530
Ninjachicken784 <chickenboi784@gmail.com>
2026-03-13 13:06:26 +0100

    Update SBPMod.java

--- src/main/java/sbp/SBPMod.java
+++ src/main/java/sbp/SBPMod.java
@@ -1,1 +1,13 @@
 
+package sbp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SBPMOD implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // This runs when your mod loads
+        System.out.println("SBP Mod loaded!");
+    }
+}
